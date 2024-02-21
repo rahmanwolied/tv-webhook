710930bbe2c9272e51c9d4eba64c14e739d15b4b
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-02-21 07:49:04 +0000

    moved to localhost

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 const cors = require('cors');
 
-const port = 3000;
+const port = 80;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -19,7 +19,12 @@ app.post('/webhook', async (req, res) => {
 	else if (account === 'zaman') port = 8081;
 
 	try {
-		const response = await axios.post(`http://178.16.139.13:${port}/order/webhook`, body);
+		const response = await axios.post(`http://localhost:${port}/order/webhook`, body);
+		let date = new Date();
+		let options = { timeZone: 'Asia/Dhaka', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+		let formatter = new Intl.DateTimeFormat([], options);
+		let dhakaTime = formatter.format(date);
+		console.log('Webhook handled at', dhakaTime);
 		console.log(response.data);
 		res.json(response.data);
 	} catch (error) {
